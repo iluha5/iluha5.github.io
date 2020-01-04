184a5fdce72bff6667213acb109eb3ad7daa6d2b
Ilya Rovda <i.rovda@dsec.ru>
2020-01-04 23:04:01 +0300

    fix(gitignore)

---
+++ build/precache-manifest.f247f615308b045bb2b4055fd3d6e5cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5eb12745695e3c645625cc50145abf3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f07b74d240a23bc86d96",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "56a75a5c5b8349d61f08",
+    "url": "/static/js/2.251880f0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.251880f0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f07b74d240a23bc86d96",
+    "url": "/static/js/main.89717be5.chunk.js"
+  },
+  {
+    "revision": "d00b8db95d6b1e48bb6e",
+    "url": "/static/js/runtime-main.c274d033.js"
+  }
+]);
